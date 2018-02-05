74fc5652f9c3dfda83ca6b47e68ed37606586e0a
David <499313351@qq.com>
2018-02-06 00:03:21 +0800

    添加Usual Setting中Name模块中的部分按钮

--- Gui/Rigging_Tool.py
+++ Gui/Rigging_Tool.py
@@ -17,13 +17,13 @@ class RiggingTool(qg.QDialog):
 
         self.setWindowTitle("Rigging Tool")
         self.setWindowFlags(qc.Qt.WindowStaysOnTopHint)
-        self.setMinimumHeight(500)
-        self.setMinimumWidth(400)
+        self.setFixedHeight(500)
+        self.setFixedWidth(350)
 
         # 添加tabWidget
 
         tab_widget = qg.QTabWidget()
-        tab_widget.addTab(NameTab(), "Name")
+        tab_widget.addTab(UsualSettingTab(), "Usual Setting")
         tab_widget.addTab(DeformerTab(), "Deformer Weight Manager")
         tab_widget.addTab(OtherTab(), "Other Tool")
 
@@ -32,15 +32,64 @@ class RiggingTool(qg.QDialog):
         self.setLayout(main_layout)
 
 
-# TODO：设置Name标签
-class NameTab(qg.QWidget):
+# TODO：设置Usual Setting标签
+class UsualSettingTab(qg.QWidget):
     def __init__(self):
-        super(NameTab, self).__init__()
+        super(UsualSettingTab, self).__init__()
 
-        file_name_la = qg.QLabel("File Name:")
+        # 设置整体布局
+
+        self.setLayout(qg.QVBoxLayout())
+        newname_widget = qg.QWidget()
+        newname_widget.setFixedHeight(285)
+        newname_widget.setFixedWidth(320)
+        newname_widget.setLayout(qg.QVBoxLayout())
+
+        self.layout().addWidget(newname_widget)
+        name_text_layout = qg. QHBoxLayout()
+        name_text_layout.layout().setContentsMargins(0, 0, 0, 0)
+        name_text_layout.layout().setAlignment(qc.Qt.AlignTop)
+        newname_widget.layout().addLayout(name_text_layout)
+
+        # 添加New Name 以及其文本编辑器
+
+        name_text_lb = qg.QLabel("New Name:")
+        name_le = qg.QLineEdit()
+
+        name_text_layout.addWidget(name_text_lb)
+        name_text_layout.addWidget(name_le)
+
+        # 设置默认递增方式（1-9/a-1) FIXME:记得修改，让该部分也可以置顶
+
+        ascending_layout = qg.QHBoxLayout()
+        ascending_layout.layout().setContentsMargins(0, 0, 0, 0)
+        ascending_layout.layout().setAlignment(qc.Qt.AlignTop)
+        newname_widget.layout().addLayout(ascending_layout)
+
+        ascending_lb = qg.QLabel('Ascending:')
+        ascending_combo = qg.QComboBox()
+        ascending_combo.addItem('Numbers (0-9)')
+        ascending_combo.addItem('Letters (a-z)')
+        ascending_combo.setFixedWidth(100)
+        ascending_layout.addWidget(ascending_lb)
+        ascending_layout.addWidget(ascending_combo)
+
+        # TODO: 添加No.Padding:和 数字选择框以及大小写切换radio
+
+        # TODO：两个check、两个LineEdit（Prefix、Suffix）
+
+        # TODO：Rename按钮
+
+        # TODO：find和replace两个labe 和对应的两个LIneEdit
+
+        # TODO：选择模式的两个Ridio（All、Selected)
+
+        # TODO：Replace按钮
+
+        # TODO: Ctrl Maker
 
         main_layout = qg.QVBoxLayout()
-        main_layout.addWidget(file_name_la)
+        main_layout.addWidget(newname_widget)
         self.setLayout(main_layout)
 
 
